a18e9cba3644c953a380395dcd0cabfe0dacf1a6
Aaron <34659376+aaronmhr@users.noreply.github.com>
2023-11-28 22:34:16 +0100

    Initial Ruta iOS app creation

---
+++ Ruta/Ruta/RutaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RutaApp.swift
+//  Ruta
+//
+//  Created by Aaron on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RutaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
