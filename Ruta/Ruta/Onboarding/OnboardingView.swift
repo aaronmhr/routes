a7915a004663f76fdd221598675128a89b314c73
Aaron <34659376+aaronmhr@users.noreply.github.com>
2023-11-30 02:50:30 +0100

    Add Onboarding View

---
+++ Ruta/Ruta/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Ruta
+//
+//  Created by Aaron on 30/11/2023.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Color.yellow
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
