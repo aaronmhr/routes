bc9c466b6af89e314b00447ea13c76dd31b49aee
Aaron <34659376+aaronmhr@users.noreply.github.com>
2023-12-03 17:17:01 +0100

    Prepare for run LaunchViewModel tests

---
+++ Ruta/RutaTests/Launch/LaunchViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchViewModelTests.swift
+//  RutaTests
+//
+//  Created by Aaron on 03/12/2023.
+//
+
+import XCTest
+
+final class LaunchViewModelTests: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
